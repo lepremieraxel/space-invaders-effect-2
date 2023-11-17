244a15a1def7b168d8d1e99f4cfc4f8fe6aab974
oihan desperben <oihandesp@gmail.com>
2023-11-17 11:28:48 +0100

    finalisation mouvements et implementation assets

--- Assets/Scripts/SpaceShipManager.cs
+++ Assets/Scripts/SpaceShipManager.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.TextCore.Text;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class SpaceShipManager : MonoBehaviour
 {
@@ -20,6 +22,13 @@ public class SpaceShipManager : MonoBehaviour
 
     public bool canControlShip;
 
+
+    //INPUTS
+    public ActionBasedController controller = null; // glisser le controler ici
+    //public float JoystickValue;
+    private bool isTriggered;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,23 +38,55 @@ public class SpaceShipManager : MonoBehaviour
 
         max_X_Left = limitX_Left.GetComponent<Transform>().position.x;
         max_X_Right = limitX_Right.GetComponent<Transform>().position.x;
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
+
     }
 
     private void FixedUpdate()
     {
+        float triggerValue = controller.translateAnchorAction.action.ReadValue<Vector2>().x;
+
+        // SPACESHIP MOVEMENTS WITH VR LEFT JOYSTICK
+        if (canControlShip == true)
+        {
+            Debug.Log(triggerValue);
+            //VR Input to move spaceshift to the left
+            if (canGoLeft == true && (triggerValue < -0.5) && !isTriggered)
+            {
+                SpaceShip.transform.Translate(-speed * Time.deltaTime, 0, 0);
+                isTriggered = true;
+            }
+            else if ((triggerValue > 0) && isTriggered)
+            {
+                isTriggered = false;
+            }
+
+            //VR Input to move spaceshift to the right
+            if (canGoRight == true && (triggerValue > 0.5) && !isTriggered)
+            {
+                SpaceShip.transform.Translate(speed * Time.deltaTime, 0, 0);
+                isTriggered = true;
+            }
+            else if ((triggerValue < 0) && isTriggered)
+            {
+                isTriggered = false;
+            }
+        }
+        
+        // SPACESHIP MOVEMENTS WITH KEYBOARD
         if (canControlShip == true)
         {
             // SpaceShip movements to go left and right on the X axis
             if (canGoLeft == true && Input.GetKey(KeyCode.LeftArrow))
             {
                 SpaceShip.transform.Translate(-speed * Time.deltaTime, 0, 0);
-                
+  
             }
             if (canGoRight == true && Input.GetKey(KeyCode.RightArrow)) 
             {
@@ -74,7 +115,5 @@ public class SpaceShipManager : MonoBehaviour
                 canGoRight = true;
             }
         }
-        
-
     }
 }
